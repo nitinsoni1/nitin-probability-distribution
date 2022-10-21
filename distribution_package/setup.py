2c62e18e5e719e2a2eea7e68dd88274ea60869c7
Nitin Soni <yonitinsoni@gmail.com>
2022-10-21 11:20:51 +0530

    Add files via upload

---
+++ distribution_package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nitin_probability_distribution',
+      version="1.2", 
+        description='Gaussian and Binomial distributions',
+        packages=['nitin_probability_distribution'],
+        author='Nitin Soni',
+        author_email='yonitinsoni@gmail.com', 
+    zip_safe=False)
+
+
